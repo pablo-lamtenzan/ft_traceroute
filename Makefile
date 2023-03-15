64cb24f9428d6987e0a8437626232f8afebe055e
Pablo <plamtenz@student.42.fr>
2023-03-15 17:05:16 +0100

    fixup entrypoint.sh

--- Makefile
+++ Makefile
@@ -1,12 +1,14 @@
+
 NAME		=		ft_traceroute
-OBJDIR		=		bin_objs
+OBJDIR		=		relocs
 CC			=		/usr/bin/gcc
 RM			=		/bin/rm
 
 include				srcs.mk
 
-CFLAGS		=		-Wall -Wextra -Werror -fsanitize=address
+CFLAGS		=		-Wall -Wextra -Werror #-g3 -fsanitize=address
 IFLAGS		=		-I$(INCDIR)
+SFLAGS		=		
 
 OBJS		=		$(patsubst $(SRCDIR)/%.c, $(OBJDIR)/%.o, $(SRCS))
 
@@ -14,14 +16,14 @@ all: $(NAME)
 	@:
 
 $(NAME) : $(OBJDIR) $(OBJS) $(HDRS)
-	@$(CC) -o $(NAME) $(CFLAGS) $(OBJS)
+	@$(CC) -o $(NAME) $(CFLAGS) $(OBJS) $(SFLAGS)
 	@echo LINK $@
 
 $(OBJDIR):
 	@mkdir -p $@
 	@echo MKDIR $@
 
-$(OBJDIR)/%.o : $(SRCDIR)/%.c 
+$(OBJDIR)/%.o : $(SRCDIR)/%.c
 	@mkdir -p $(shell dirname $@)
 	@$(CC) -c -o $@ $(CFLAGS) $(IFLAGS) $<
 	@echo CC $<
--- srcs.mk
+++ srcs.mk
@@ -1,35 +1,35 @@
 INCDIR	=	includes
 SRCDIR	=	srcs
 
-HDRS	=\
-$(addprefix includes/,\
-	ft_error.h\
-	ftlibc.h\
-	gcc_dependent.h\
-	host.h\
-	parse.h\
-	parse_types.h\
-	traceroute.h\
+HDRS	= \
+$(addprefix includes/, \
+	ft_error.h \
+	ftlibc.h \
+	gcc_dependent.h \
+	host.h \
+	parse.h \
+	parse_types.h \
+	traceroute.h \
 )
-SRCS	=\
-$(addprefix srcs/,\
-	$(addprefix ftlibc/,\
-		atol.c\
-		memcpy.c\
-		memset.c\
-		strlen.c\
-		strncmp.c\
-	)\
-	gethostinfo.c\
-	host.c\
-	init_socket.c\
-	$(addprefix legacy/,\
-		cksum.c\
-		tvsub.c\
-	)\
-	main.c\
-	parse_opts.c\
-	print_route.c\
-	receivprobe.c\
-	sendprobe.c\
+SRCS	= \
+$(addprefix srcs/, \
+	$(addprefix ftlibc/, \
+		atol.c \
+		memcpy.c \
+		memset.c \
+		strlen.c \
+		strncmp.c \
+	) \
+	gethostinfo.c \
+	host.c \
+	init_socket.c \
+	$(addprefix legacy/, \
+		cksum.c \
+		tvsub.c \
+	) \
+	main.c \
+	parse_opts.c \
+	print_route.c \
+	receivprobe.c \
+	sendprobe.c \
 )
